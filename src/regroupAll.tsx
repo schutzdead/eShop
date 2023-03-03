cb995ab5747beba536b8e3f48d13cbf7fa1b7105
Lucas <lucas.soubry@gmail.com>
2023-03-03 13:39:48 +0100

    fixed build

--- src/regroupAll.tsx
+++ src/regroupAll.tsx
@@ -2,18 +2,16 @@ import { useState } from 'react'
 import App from './components_first/App'
 import Shop from './components_shop/Shop'
 import './index.css'
-import { HashRouter, Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 
 export function RegroupAll () {
     
     const [yourCard, setYourCard] = useState([])
 
     return(
-    <HashRouter basename='/eShop'>
     <Routes>
       <Route path="/" element={<App yourCard={yourCard} setYourCard={setYourCard}/>} />
       <Route path="/shop" element={<Shop yourCard={yourCard} setYourCard={setYourCard}/>} />
     </Routes>
-  </HashRouter>
     )
 }

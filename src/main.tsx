6a33ff7a883718e69013f358abaa01afe2b11d15
Lucas <lucas.soubry@gmail.com>
2023-03-03 15:11:24 +0100

    fixed build vite

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RegroupAll } from './regroupAll'
-import { HashRouter} from "react-router-dom"
-
-
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   
   <React.StrictMode>
-    <HashRouter>
       <RegroupAll />
-    </ HashRouter>
   </React.StrictMode>
 )
--- src/regroupAll.tsx
+++ src/regroupAll.tsx
@@ -2,16 +2,18 @@ import { useState } from 'react'
 import App from './components_first/App'
 import Shop from './components_shop/Shop'
 import './index.css'
-import { Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 export function RegroupAll () {
     
     const [yourCard, setYourCard] = useState([])
 
     return(
-    <Routes>
-      <Route path="/" element={<App yourCard={yourCard} setYourCard={setYourCard}/>} />
-      <Route path="/shop" element={<Shop yourCard={yourCard} setYourCard={setYourCard}/>} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App yourCard={yourCard} setYourCard={setYourCard}/>} />
+        <Route path="/shop" element={<Shop yourCard={yourCard} setYourCard={setYourCard}/>} />
+      </Routes>
+    </BrowserRouter>
     )
 }
